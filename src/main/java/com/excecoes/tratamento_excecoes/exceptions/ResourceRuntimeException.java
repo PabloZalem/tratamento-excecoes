faba36cb25a1356ae1b9e523c7dcc5335a91149f
Pablo Zalem <zalem.pablo@yahoo.com>
2025-11-04 19:50:21 -0300

    Classe error customizada

---
+++ src/main/java/com/excecoes/tratamento_excecoes/exceptions/ResourceRuntimeException.java
@@ -0,0 +1,8 @@
+package com.excecoes.tratamento_excecoes.exceptions;
+
+public class ResourceRuntimeException extends RuntimeException {
+    public ResourceRuntimeException(String message) {
+        super(message);
+    }
+    
+}
