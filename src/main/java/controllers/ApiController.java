973424c5d28b75ce1b909c8ceebba58b890e9b97
Pablo Zalem <zalem.pablo@yahoo.com>
2025-11-04 18:35:14 -0300

    Criamos nosso controller

--- src/main/java/controllers/ApiController.java
+++ src/main/java/controllers/ApiController.java
@@ -1,8 +1,13 @@
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
-    
+
+    @GetMapping("/hello")
+    public String Hello() {
+        return "Hello, World!";
+    }
 }
