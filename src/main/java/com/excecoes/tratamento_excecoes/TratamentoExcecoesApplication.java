a4de75d0cbea3ec92330c19598dde5d1dbbfc9b6
Pablo Zalem <zalem.pablo@yahoo.com>
2025-11-04 18:27:48 -0300

    Aprofundando no estudo de excecoes

---
+++ src/main/java/com/excecoes/tratamento_excecoes/TratamentoExcecoesApplication.java
@@ -0,0 +1,13 @@
+package com.excecoes.tratamento_excecoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TratamentoExcecoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TratamentoExcecoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excecoes/tratamento_excecoes/TratamentoExcecoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excecoes.tratamento_excecoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TratamentoExcecoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
