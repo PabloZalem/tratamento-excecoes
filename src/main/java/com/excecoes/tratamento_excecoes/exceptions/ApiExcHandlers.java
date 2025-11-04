15d59bcd6a4ebaad70574b38ca40dba7e1cab491
Pablo Zalem <zalem.pablo@yahoo.com>
2025-11-04 19:50:55 -0300

    Sem alteracoes

--- src/main/java/com/excecoes/tratamento_excecoes/exceptions/ApiExcHandlers.java
+++ src/main/java/com/excecoes/tratamento_excecoes/exceptions/ApiExcHandlers.java
@@ -1,5 +1,6 @@
 package com.excecoes.tratamento_excecoes.exceptions;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
